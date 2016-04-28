229f8c57d3fa1fad8e844db880ec994888b34631
Deena Wang <thedeenawang@gmail.com>
2016-04-27 21:43:09 -0400

    Generate some stock responses

--- src/generateBoyResponse.js
+++ src/generateBoyResponse.js
@@ -1,4 +1,16 @@
+var responses = [
+  'tits or gtfo',
+  'show boobs',
+  'gimme kiss',
+  'pleas?',
+  'ur hot'
+]
+
+function getRandomItemFromList(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
 // Given a message, returns a response
 module.exports = function(message) {
-  return "Echo: " + message.substring(0, 200);
+  return getRandomItemFromList(responses);
 }
