e6f201fe9ae3987ad91de30cf225ec1e93b6d9a8
Deena Wang <thedeenawang@gmail.com>
2016-06-01 21:19:04 -0400

    Methods for saving an image

--- src/scripts/scrapeTumblr.py
+++ src/scripts/scrapeTumblr.py
@@ -1,10 +1,18 @@
 import requests
+from urlparse import urlparse
+import os.path
+
+from PIL import Image
+from StringIO import StringIO
 
 from bs4 import BeautifulSoup
 from lxml import html
 
-def scrape_page(pageNum):
-
+def get_image_urls_on_page(pageNum):
+    '''
+    Returns the url of all images from a page on tumblr
+    pageNum - an integer of page to scrape. one-indexed
+    '''
     page = requests.get('http://straightwhiteboystexting.org/page/' + str(pageNum))
     tree = html.fromstring(page.content)
 
@@ -13,6 +21,18 @@ def scrape_page(pageNum):
     for image in images:
         image_urls.append(image.get('src'))
 
-    print image_urls
+    return image_urls
 
-scrape_page(2)
+def load_image(url):
+    image_request = requests.get(url)
+    image = Image.open(StringIO(image_request.content))
+    file_name = get_image_name(url)
+    image.save('screenshots/' + file_name, 'JPEG')
+
+def get_image_name(url):
+    path = urlparse(url).path
+    directory, file = os.path.split(path)
+    return file
+
+image_urls = get_image_urls_on_page(1)
+load_image(image_urls[0])
