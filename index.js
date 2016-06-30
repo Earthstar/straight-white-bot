83e85f58c3bee253d2c42f87f5b3fe901fc2ce41
Deena Wang <thedeenawang@gmail.com>
2016-06-30 15:11:05 -0400

    Add state machine to response generation

--- index.js
+++ index.js
@@ -6,78 +6,72 @@ var generateBoyResponse = require('./src/generateBoyResponse');
 
 var app = express();
 
-// You MUST change these values, consult the Messenger Platform Getting Started guide
 var verify_token = process.env.VERIFY_TOKEN;
 var token = process.env.PAGE_ACCESS_TOKEN;
-
 var PORT = process.env.PORT || 1337;
 
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 
-    res.send('Hello World! This is the bot\'s root endpoint!');
+  res.send('Hello World! This is the bot\'s root endpoint!');
 
 });
 
 app.get('/webhook/', function (req, res) {
 
-    if (req.query['hub.verify_token'] === verify_token) {
-        res.send(req.query['hub.challenge']);
-    }
+  if (req.query['hub.verify_token'] === verify_token) {
+    res.send(req.query['hub.challenge']);
+  }
 
-    res.send('Error, wrong validation token');
+  res.send('Error, wrong validation token');
 
 });
 
 app.post('/webhook/', function (req, res) {
 
-    var messaging_events = req.body.entry[0].messaging;
+  var messaging_events = req.body.entry[0].messaging;
 
-    for (var i = 0; i < messaging_events.length; i++) {
+  for (var i = 0; i < messaging_events.length; i++) {
 
-        var event = req.body.entry[0].messaging[i];
-        var sender = event.sender.id;
+    var event = req.body.entry[0].messaging[i];
+    var senderId = event.sender.id;
 
-        if (event.message && event.message.text) {
-            var text = event.message.text;
-
-            sendTextMessage(sender, generateBoyResponse(text));
-        }
+    if (event.message && event.message.text) {
+      var text = event.message.text;
+      sendTextMessage(senderId, generateBoyResponse(text));
     }
+  }
 
-    res.sendStatus(200);
+  res.sendStatus(200);
 
 });
 
 app.listen(PORT, function () {
-
-    console.log('Facebook Messenger echoing bot started on port:' + PORT);
-
+  console.log('Facebook Messenger echoing bot started on port:' + PORT);
 });
 
-function sendTextMessage(sender, text) {
+function sendTextMessage(senderId, text) {
+  var messageData = {
+    text: text
+  };
 
-    var messageData = {
-        text: text
-    };
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token},
+    method: 'POST',
+    json: {
+      recipient: {id: senderId},
+      message: messageData
+    }
+  }, function (error, response) {
 
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: token},
-        method: 'POST',
-        json: {
-            recipient: {id: sender},
-            message: messageData
-        }
-    }, function (error, response) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
 
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-
-    });
+  });
 
 }
--- src/generateBoyResponse.js
+++ src/generateBoyResponse.js
@@ -1,16 +1,45 @@
-var responses = [
+var _ = require('lodash');
+var HOPEFUL_STATE = 'hopeful';
+var AGGRESSIVE_STATE = 'aggressive';
+// Dictonary of sender to state in state machine
+// of form senderId: {state: 'HOPEFUL'}
+var senderStateDict = {};
+
+function getSenderState(senderId) {
+  var senderState = senderStateDict[senderId];
+  if (_.isUndefined(senderState)) {
+    senderStateDict[senderId] = HOPEFUL_STATE;
+    senderState = HOPEFUL_STATE;
+  }
+  return senderState;
+}
+
+
+var hopefulResponses = [
   'tits or gtfo',
   'show boobs',
   'gimme kiss',
   'pleas?',
   'ur hot'
-]
+];
+
+var aggresiveResponses = [
+  '*THREAT OF SEXUAL VIOLENCE*'
+];
+
 
 function getRandomItemFromList(list) {
   return list[Math.floor(Math.random() * list.length)];
 }
 
 // Given a message, returns a response
-module.exports = function(message) {
-  return getRandomItemFromList(responses);
-}
+module.exports = function(senderId, message) {
+  var senderState = getSenderState(senderId);
+  if (senderState === HOPEFUL_STATE) {
+    return getRandomItemFromList(hopefulResponses);
+  } else if (senderState === AGGRESSIVE_STATE) {
+    return getRandomItemFromList(aggresiveResponses);
+  } else {
+    return 'Sender does not have a valid state: ' + senderState;
+  }
+};
