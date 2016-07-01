06ea5a3b16dc9eb6271bdd73871d0484a5d14d99
Deena Wang <thedeenawang@gmail.com>
2016-07-01 14:21:23 -0400

    Update message sending based on new fb example

--- index.js
+++ index.js
@@ -39,7 +39,12 @@ app.post('/webhook/', function (req, res) {
     // There may be multiple if batched
     data.entry.forEach(function(pageEntry) {
       pageEntry.messaging.forEach(function(messagingEvent) {
-        console.log(messagingEvent);
+        if (messagingEvent.message) {
+          console.log(messagingEvent);
+          handleMessageEvent(messagingEvent);
+        } else {
+          console.log("Received messaging event that isn't handled", messagingEvent);
+        }
       });
     });
   }
@@ -68,27 +73,74 @@ app.listen(PORT, function () {
   console.log('Facebook Messenger echoing bot started on port:' + PORT);
 });
 
-function sendTextMessage(senderId, text) {
+// Handles FB messaging events of type 'message'
+function handleMessageEvent(event) {
+  var senderId = event.sender.id;
+  var message = event.message;
+  // for now, only handle text messages
+  var messageText = message.text;
+
+  // echo for now
+  sendTextMessage(messageText);
+}
+
+// The recipient of the message will be the user who 'sent' the message
+function sendTextMessage(recipientId, messageText) {
   var messageData = {
-    text: text
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
   };
 
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: token},
-    method: 'POST',
-    json: {
-      recipient: {id: senderId},
-      message: messageData
-    }
-  }, function (error, response) {
-
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-
-  });
-
+  callSendAPI(messageData);
 }
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: token },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
+
+// function sendTextMessage(senderId, text) {
+//   var messageData = {
+//     text: text
+//   };
+
+//   request({
+//     url: 'https://graph.facebook.com/v2.6/me/messages',
+//     qs: {access_token: token},
+//     method: 'POST',
+//     json: {
+//       recipient: {id: senderId},
+//       message: messageData
+//     }
+//   }, function (error, response) {
+
+//     if (error) {
+//       console.log('Error sending message: ', error);
+//     } else if (response.body.error) {
+//       console.log('Error: ', response.body.error);
+//     }
+
+//   });
+
+// }
